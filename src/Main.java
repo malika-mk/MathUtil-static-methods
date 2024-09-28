d1f25821879607545cbda65375f7d2ef4dd0a6e0
Malika <malikamkkambay@gmail.com>
2024-09-28 23:13:27 +0600

    first c

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+public class Main {
+    // Main method to test the class
+    public static void main(String[] args) {
+        // Testing Static Methods
+        System.out.println("Static Methods:");
+
+        // 1. Check if a number is prime
+        System.out.println("isPrime(7): " + MathUtil.isPrime(7));
+        System.out.println("isPrime(4): " + MathUtil.isPrime(4));
+        System.out.println("isPrime(11): " + MathUtil.isPrime(11));
+
+        // 2. GCD of two numbers
+        System.out.println("gcd(54, 24): " + MathUtil.gcd(54, 24));
+        System.out.println("gcd(10, 110): " + MathUtil.gcd(10, 110));
+        System.out.println("gcd(40, 36): " + MathUtil.gcd(40, 36));
+
+        // 3. LCM of two numbers
+        System.out.println("lcm(5, 25): " + MathUtil.lcm(5, 25));
+        System.out.println("lcm(10, 33): " + MathUtil.lcm(10, 33));
+        System.out.println("lcm(7, 22): " + MathUtil.lcm(7, 22));
+
+        // 4. Fibonacci number (nth)
+        System.out.println("fibonacci(7): " + MathUtil.fibonacci(7));
+        System.out.println("fibonacci(5): " + MathUtil.fibonacci(5));
+        System.out.println("fibonacci(1): " + MathUtil.fibonacci(1));
+
+        // 5. Factorial of a number
+        System.out.println("factorial(10): " + MathUtil.factorial(10));
+        System.out.println("factorial(5): " + MathUtil.factorial(5));
+        System.out.println("factorial(4): " + MathUtil.factorial(4));
+
+
+
+        // Create an object for non-static methods
+        MathUtil mathUtil = new MathUtil();
+
+        // 1. Check if a number is a perfect number
+        System.out.println("isPerfectNumber(28): " + mathUtil.isPerfectNumber(28));
+        System.out.println("isPerfectNumber(8): " + mathUtil.isPerfectNumber(8));
+        System.out.println("isPerfectNumber(36): " + mathUtil.isPerfectNumber(36));
+
+        // 2. Sum of digits of a number
+        System.out.println("sumOfDigits(123): " + mathUtil.sumOfDigits(123));
+        System.out.println("sumOfDigits(6767): " + mathUtil.sumOfDigits(6767));
+        System.out.println("sumOfDigits(3465): " + mathUtil.sumOfDigits(3465));
+
+        // 3. Reverse the digits of a number
+        System.out.println("reverseNumber(2006): " + mathUtil.reverseNumber(2006));
+        System.out.println("reverseNumber(9944): " + mathUtil.reverseNumber(9944));
+        System.out.println("reverseNumber(1818): " + mathUtil.reverseNumber(1818));
+
+        // 4. Checks if a number is an Armstrong number.
+        System.out.println("isArmstrongNumber(153): " + mathUtil.isArmstrongNumber(153));
+        System.out.println("isArmstrongNumber(144): " + mathUtil.isArmstrongNumber(144));
+        System.out.println("isArmstrongNumber(188): " + mathUtil.isArmstrongNumber(188));
+
+        // 5. Finds the smallest prime number greater than n.
+        System.out.println("nextPrime(14): " + mathUtil.nextPrime(14));
+        System.out.println("nextPrime(111): " + mathUtil.nextPrime(111));
+        System.out.println("nextPrime(2024): " + mathUtil.nextPrime(2024));
+
+
     }
 }
--- src/MathUtil.java
+++ src/MathUtil.java
@@ -1,19 +1,111 @@
 public class MathUtil {
 
+    // 5 Static Methods
 
-        public static boolean isPrime(int n) {
-            int i, j;
-            for(int i = 2; i <= 100; i ++){
-                boolean isPrime = true;
+    // 1. Check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
 
-                if(i % 2 == 0){
-                    isPrime = false;
+    // 2. GCD of two numbers
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 
-                }
-                if(isPrime){
-                    System.out.println(i);
-                }
+    // 3. LCM of two numbers
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    // 4. Fibonacci number (nth)
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1, fib = 0;
+        for (int i = 2; i <= n; i++) {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+        return fib;
+    }
+
+    // 5. Factorial of a number
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    //  Non-static Methods
+
+    // 1. Check if a number is a perfect number
+    public boolean isPerfectNumber(int n) {
+        if (n < 1) return false;
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
             }
         }
+        return sum == n;
+    }
+
+    // 2. Sum of digits of a number
+    public int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    // 3. Reverse the digits of a number
+    public static int reverseNumber(int n) {
+        int reversed = 0;
+        while (n != 0) {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
+        }
+        return reversed;
+    }
+
+//     4. Checks if a number is an Armstrong number.
+    public static boolean isArmstrongNumber(int n) {
+        int sum = 0, temp = n, digits = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = n;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+
+   //   5. Finds the smallest prime number greater than n.
+   public static int nextPrime(int n) {
+       int next = n + 1;
+       while (!isPrime(next)) {
+           next++;
+       }
+       return next;
+   }
+
 
 }
